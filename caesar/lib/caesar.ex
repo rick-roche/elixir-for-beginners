4b46279947dc52914a4d86a4a94764aafe93c7ca
Richard Roché <richard.j.roche@gmail.com>
2017-12-27 15:30:21 +0200

    feat: added escript

--- caesar/lib/caesar.ex
+++ caesar/lib/caesar.ex
@@ -2,17 +2,30 @@ defmodule Caesar do
   @moduledoc """
   Documentation for Caesar.
   """
+  def main(argv) do
+    argv
+      |> parse_args
+      |> process
+    System.halt(0)
+  end
 
-  @doc """
-  Hello world.
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean])
+    case parse do
+      {[help: true], __________________, _} -> {:help}
+      {[shift: shift], ["encrypt", msg], _} -> {:encrypt, msg, shift |> String.to_integer}
+      _____________________________________ -> {:help}
+    end
+  end
 
-  ## Examples
+  defp process({:encrypt, msg, shift}) do
+    Caesar.Cipher.encrypt(msg, shift)
+      |> IO.puts
+  end
 
-      iex> Caesar.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp process({:help}) do
+    IO.puts """
+    usage: ./caesar <command> <message> --shift <shift_number>
+    """
   end
 end
--- caesar/test/caesar_test.exs
+++ caesar/test/caesar_test.exs
@@ -2,7 +2,13 @@ defmodule CaesarTest do
   use ExUnit.Case
   doctest Caesar
 
-  test "greets the world" do
-    assert Caesar.hello() == :world
+  test "parse_args help" do
+    argv = ["help"]
+    assert Caesar.parse_args(argv) == {:help}
+  end
+
+  test "parse_args encrypt" do
+    argv = ["encrypt", "abc", "--shift", "1"]
+    assert Caesar.parse_args(argv) == {:encrypt, "abc", 1}
   end
 end
