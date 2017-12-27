14ae1f9abb0307723f2b5db4362a1d93ac2df7f2
Richard Roché <richard.j.roche@gmail.com>
2017-12-27 13:36:58 +0200

    feat: basic recursion added

---
+++ recursion.exs
@@ -0,0 +1,11 @@
+## Recursion
+
+defmodule Recursion do
+    def countdown(0), do: IO.puts "Blast off!"
+    def countdown(n) do
+        IO.puts n
+        countdown(n - 1)
+    end
+end
+
+Recursion.countdown(5)
