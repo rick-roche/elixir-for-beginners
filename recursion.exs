2b54c365ec9fd6cbdde5f1e432db3f23fc0b0ff5
Richard Roché <richard.j.roche@gmail.com>
2017-12-27 13:39:41 +0200

    feat: added guard clauses

--- recursion.exs
+++ recursion.exs
@@ -2,10 +2,16 @@
 
 defmodule Recursion do
     def countdown(0), do: IO.puts "Blast off!"
-    def countdown(n) do
+    def countdown(n) when n == 2 do
+        IO.puts "#{n}...ignite engines..."
+        countdown(n - 1)
+    end
+    def countdown(n) when is_integer n do
         IO.puts n
         countdown(n - 1)
     end
+    def countdown(str) when is_binary(str), do: IO.puts "C'mon... countdown is a number!"
 end
 
 Recursion.countdown(5)
+Recursion.countdown("ten")
